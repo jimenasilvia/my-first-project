9018898917a45550baa60cfa29b055c2aa99c9ab
jimena <jime_jse@hotmail.com>
2016-11-02 15:39:50 -0500

    Mi app Django hotel, segundo commit

--- apphotel/admin.py
+++ apphotel/admin.py
@@ -1,3 +1,80 @@
 from django.contrib import admin
+from .models import Customer,Room_type,Room,Rating,Room_rating,Booking,Rent,Bill,Pay_type,Bill_payment
 
 # Register your models here.
+
+class AdminCustomer(admin.ModelAdmin):
+	list_display = ['dni','name','firstname','lastname','email','telephone']
+	class Meta:
+		model = Customer
+
+admin.site.register(Customer,AdminCustomer)
+
+
+class AdminRoom_type(admin.ModelAdmin):
+	list_display = ['name_type','description']
+	class Meta:
+		model = Room_type
+
+admin.site.register(Room_type,AdminRoom_type)
+
+class AdminRating(admin.ModelAdmin):
+	list_display = ['description']
+	class Meta:
+		model = Rating
+
+admin.site.register(Rating,AdminRating)
+
+class AdminPay_type(admin.ModelAdmin):
+	list_display = ['pay_type']
+	class Meta:
+		model = Pay_type
+
+admin.site.register(Pay_type,AdminPay_type)
+
+
+class AdminRoom(admin.ModelAdmin):
+	list_display = ['nro_room', 'name_type','is_reserved','booking']
+	class Meta:
+		model = Room
+
+admin.site.register(Room,AdminRoom)
+
+
+class AdminRoom_rating(admin.ModelAdmin):
+	list_display = ['room','rating','from_date','to_date','is_active']
+	class Meta:
+		model = Room_rating
+
+admin.site.register(Room_rating,AdminRoom_rating)
+
+class AdminBooking(admin.ModelAdmin):
+	list_display = ['admin','customer','reservation_date','in_date','out_date']
+	class Meta:
+		model = Booking
+
+admin.site.register(Booking,AdminBooking)
+
+class AdminRent(admin.ModelAdmin):
+	list_display = ['name_type','amount','from_date','to_date','is_active']
+	class Meta:
+		model = Rent
+
+admin.site.register(Rent,AdminRent)
+
+class AdminBill(admin.ModelAdmin):
+	list_display = ['booking','rent','total']
+	class Meta:
+		model = Bill
+
+admin.site.register(Bill,AdminBill)
+
+class AdminBill_payment(admin.ModelAdmin):
+	list_display = ['bill','pay_type','payment_date']
+	class Meta:
+		model = Bill_payment
+
+admin.site.register(Bill_payment,AdminBill_payment)
+
+
+
--- apphotel/models.py
+++ apphotel/models.py
@@ -2,68 +2,92 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.conf import settings
+
+
 # Create your models here.
-
-"""class Rol (models.Model):
-	rol=models.CharField()
-	
-class User(models.Model):
-	User=models.CharField()
-	password=models.CharField()
-	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-	actualizado = models.DateTimeField(auto_now_add=False, auto_now=True)
-
-
-class Admin(models.Model):
-	user=models.CharField()
-	dni=models.CharField()
-	name=models.CharField()
-	firstname=models.CharField()
-	lastname}=models.CharField()
-	email=models.EmailField()
-	telephone=models.CharField()
-	direction=models.CharField()
-
 class Customer(models.Model):
-	user=models.CharField()
-	dni=models.CharField()
-	name=models.CharField()
-	firstname=models.CharField()
-	lastname}=models.CharField()
-	email=models.EmailField()
-	telephone=models.CharField()
+	dni=models.CharField(max_length=8)
+	name=models.CharField(max_length=50)
+	firstname=models.CharField(max_length=30)
+	lastname=models.CharField(max_length=30)
+	email=models.EmailField(max_length=256)
+	telephone=models.CharField(max_length=15,blank=True)
 
-class Type_room(models.Model):
-	name_type=models.CharField()
-	description=models.TextField()
-class Room(models.Model):
-	nro_room=models.CharField()
-	name_type=models.ForeignKey(Type_Room)
-	is_reserved=models.BooleanField()
+	def __unicode__(self):
+		 return '%s (%s %s)' % (self.dni, self.name,self.firstname)
+
+class Room_type(models.Model):
+	name_type=models.CharField('Room Type',max_length=15)
+	description=models.TextField(blank=True)
+
+	def __unicode__(self):
+		return self.name_type
+
+class Pay_type(models.Model):
+	pay_type=models.CharField(max_length=12)
+
+	def __unicode__(self):
+		return self.pay_type
+
 class Rating(models.Model):
 	description=models.TextField()
-class Room_rating(models.Model)
-     
 
-
-
-class Booking(models.Model):
-    admin=models.ForeignKey(Admin)
-    customer=models.ForeignKey(Customer)
-    room=models.ForeignKey(Room)
-    in_date=models.DateTimeField()
-    out_date=models.DateTimeField()
+	def __unicode__(self):
+		return self.description
 
 class Rent(models.Model):
-    name_type=models.ForeignKey(Type_Room)
-    amount=models.DecimalField()
+    name_type=models.ForeignKey(Room_type,verbose_name='Room Type')
+    amount=models.DecimalField(max_digits=6, decimal_places=3)
     from_date=models.DateTimeField()
     to_date=models.DateTimeField()
     is_active=models.BooleanField()
 
+    def __unicode__(self):
+		return '%s' % (self.name_type)
 
-"""
+
+class Booking(models.Model):
+    admin=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.DO_NOTHING,default=1)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    in_date=models.DateTimeField()
+    out_date=models.DateTimeField()
+    reservation_date=models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '%s (%s)' % (self.customer, self.admin)
 
 
+class Room(models.Model):
+	nro_room=models.CharField(max_length=3)
+	name_type=models.ForeignKey(Room_type,verbose_name='Room Type')
+	is_reserved=models.BooleanField()
+	booking=models.ForeignKey(Booking,blank=True,null=True,on_delete=models.DO_NOTHING)
+
+
+	def __unicode__(self):
+		return self.nro_room
+
+
+class Room_rating(models.Model):
+     room=models.ForeignKey(Room,verbose_name='Room',default=1)
+     rating=models.ForeignKey(Rating,blank=True,null=True,on_delete=models.DO_NOTHING)
+     from_date=models.DateTimeField()
+     to_date=models.DateTimeField()
+     is_active=models.BooleanField()
+
+class Bill(models.Model):
+	booking=models.ForeignKey(Booking)
+	rent=models.ForeignKey(Rent)
+	total=models.DecimalField(max_digits=9,decimal_places=2)
+
+	def __unicode__(self):
+		return '%s' % (self.booking)
+
+class Bill_payment(models.Model):
+	bill=models.ForeignKey(Bill,blank=True,null=True,on_delete=models.DO_NOTHING)
+	pay_type=models.ForeignKey(Pay_type,blank=True,null=True,on_delete=models.DO_NOTHING)
+	payment_date=models.DateTimeField(auto_now_add=True)
 	
-
+	def __unicode__(self):
+		return '%s' % (self.bill)
